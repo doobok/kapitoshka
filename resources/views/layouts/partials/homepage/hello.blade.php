cfaab80301d2a62a60438530756cec2b7da50afa
doobok <38143500+doobok@users.noreply.github.com>
2024-04-06 08:54:17 +0300

    Update hello.blade.php

--- resources/views/layouts/partials/homepage/hello.blade.php
+++ resources/views/layouts/partials/homepage/hello.blade.php
@@ -1,5 +1,5 @@
 <div class="uk-container uk-container-small uk-flex-auto uk-text-center uk-section uk-section-xlarge" data-uk-scrollspy="target: > .animate; cls: uk-animation-slide-bottom-small uk-invisible; delay: 300">
-  <p class="uk-h3 animate uk-invisible uk-margin-remove-top">Дошкільний навчальний заклад (ясла-садок) комбінованого типу №5</p>
+  <p class="uk-h3 animate uk-invisible uk-margin-remove-top">setting('site.description')</p>
   <h1 class="uk-heading-xlarge animate uk-invisible uk-margin-remove-top">Капіт<i class="fab fa-opera"></i>шка</h1>
   <div class="uk-width-4-5@m uk-margin-auto animate uk-invisible">
     <p class="uk-text-lead">Ми завжди готові прийняти нових  друзів, розділити з ними радощі і турботи, зігріти теплом маленькі серця!</p>
